4e568d11401d5d56714cbbf6637fccfe327ab835
Daniel Clark <dclark253@outlook.com>
2023-06-25 19:52:30 -0700

    Created Model, Controller, Service, and Repository Classes to implement basic GET and POST endpoints

---
+++ src/main/java/com/pearson/movies/service/MovieService.java
@@ -0,0 +1,23 @@
+package com.pearson.movies.service;
+
+import com.pearson.movies.model.Movie;
+import com.pearson.movies.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+}
